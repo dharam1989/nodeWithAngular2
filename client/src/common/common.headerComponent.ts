f52f4fd9da26693d04650f442da0ee7d95412f5e
Dharmendra <dharmendra.thakur@webdunia.net>
2017-06-21 13:23:10 +0530

    Angular & Node have same package.json

--- client/src/common/common.headerComponent.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'header-root',
-  templateUrl: './common.headerComponent.html',
-  styleUrls: ['./common.headerComponent.css']
-})
-export class HeaderComponent {
-}
